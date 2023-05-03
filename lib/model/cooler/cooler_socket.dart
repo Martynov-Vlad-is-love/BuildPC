4f6190a9452da2475a189476e31cf9ef1eea8abd
Martynov-Vlad-is-love <vladislav.martynov.0@gmail.com>
2023-05-03 13:18:37 +0300

    Add json methods.

--- lib/model/cooler/cooler_socket.dart
+++ lib/model/cooler/cooler_socket.dart
@@ -1,6 +1,19 @@
 class CoolerSocket{
   int id;
-  String name;
+  String socket;
 
-  CoolerSocket(this.id, this.name);
+  CoolerSocket(this.id, this.socket);
+
+  CoolerSocket.fromJson(Map<String, dynamic> json)
+      : id = json["id"] as int,
+        socket = json["socket"] as String;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{
+      "id": id.toString(),
+      "socket": socket,
+    };
+
+    return data;
+  }
 }
